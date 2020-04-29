1e8860acf90a900ed6bea5d62bf646fc404451d7
abish7643 <abishvijayan7643@gmail.com>
2020-04-30 00:37:20 +0530

    404 While Refreshing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 import * as ROUTES from '../../constants/routes'
 //import { withFirebase } from '../Firebase';
