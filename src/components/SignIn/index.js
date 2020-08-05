fe8339a3516855943e7c857696072123f3a40d89
Abish <abishvijayan7643@gmail.com>
2020-08-06 00:33:53 +0530

    Updated Packages && Removed Yarn

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
-import { PasswordForgetLink } from '../PasswordForget';
-import {css} from "@emotion/core";
+import { PasswordForgetLink } from "../PasswordForget";
+import { css } from "@emotion/core";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const SignInPage = () => (
   <div className="container">
-    <h1>GET RIGHT INTO <span className="highlighted__text">ACTION</span></h1>
+    <h1>
+      GET RIGHT INTO <span className="highlighted__text">ACTION</span>
+    </h1>
     <SignInForm />
     <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
 
-
 const INITIAL_STATE = {
-  email: '',
-  password: '',
+  email: "",
+  password: "",
   error: null,
   loading: false,
 };
@@ -29,8 +30,8 @@ class SignInFormBase extends Component {
     super(props);
     this.state = { ...INITIAL_STATE };
   }
-  onSubmit = event => {
-    this.setState({loading: true})
+  onSubmit = (event) => {
+    this.setState({ loading: true });
     const { email, password } = this.state;
     this.props.firebase
       .doSignInWithEmailAndPassword(email, password)
@@ -38,45 +39,63 @@ class SignInFormBase extends Component {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({ error });
-        this.setState({loading: false})
+        this.setState({ loading: false });
       });
     event.preventDefault();
   };
-  onChange = event => {
+  onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
   render() {
     const { email, password, error, loading } = this.state;
-    const isInvalid = password === '' || email === '';
+    const isInvalid = password === "" || email === "";
     return (
       <form onSubmit={this.onSubmit} className="formcontainer">
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-          required />
-          <label for="name" className="form__label">Email Address</label>
+          <input
+            type="input"
+            className="form__field"
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+            required
+          />
+          <label for="name" className="form__label">
+            Email Address
+          </label>
         </div>
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-          required />
-        <label for="name" className="form__label">Password</label>
+          <input
+            type="input"
+            className="form__field"
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <label for="name" className="form__label">
+            Password
+          </label>
         </div>
-        <button disabled={isInvalid} className="button__form__submit" type="submit">Sign In <ClipLoader size={8} 
-        color={'#4CB8A4'} loading={loading}/></button>
-        <div className="error">{error && <p className="error__text caution__text">{error.message}</p>}</div>
+
+        <div className="error">
+          {error && (
+            <p className="error__text caution__text">{error.message}</p>
+          )}
+        </div>
+        <button
+          disabled={isInvalid}
+          className="button__form__submit"
+          type="submit"
+        >
+          Sign In <ClipLoader size={8} color={"#4CB8A4"} loading={loading} />
+        </button>
       </form>
     );
   }
@@ -84,14 +103,13 @@ class SignInFormBase extends Component {
 
 const SignUpLink = () => (
   <p className="form__bottom__links smaller__text">
-    Don't have an Account? <Link to={ROUTES.SIGN_UP} className="link__decoration">
-    <span className="highlighted__text">SIGN UP</span></Link>
+    Don't have an Account?{" "}
+    <Link to={ROUTES.SIGN_UP} className="link__decoration">
+      <span className="highlighted__text">SIGN UP</span>
+    </Link>
   </p>
 );
 
-const SignInForm = compose(
-  withRouter,
-  withFirebase,
-)(SignInFormBase);
+const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
 export default SignInPage;
 export { SignInForm, SignUpLink };
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { compose } from "recompose";
 
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
-import './signup.scss'
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
+import "./signup.scss";
 
-import {css} from "@emotion/core";
+import { css } from "@emotion/core";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const SignUpPage = () => (
   <div className="container">
-    <h1>GET STARTED <span className="highlighted__text">RIGHT AWAY</span></h1>
-    <SignUpForm/>
-    <SignInLink/>
+    <h1>
+      GET STARTED <span className="highlighted__text">RIGHT AWAY</span>
+    </h1>
+    <SignUpForm />
+    <SignInLink />
   </div>
 );
 
-
 const INITIAL_STATE = {
-  username: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
   error: null,
   loading: false,
 };
@@ -34,8 +35,7 @@ class SignUpFormBase extends Component {
     this.state = { ...INITIAL_STATE };
   }
 
-
-  onSubmit = event => {
+  onSubmit = (event) => {
     this.setState({ loading: true });
     const { username, email, passwordOne } = this.state;
     const challengesCompleted = 0;
@@ -46,20 +46,17 @@ class SignUpFormBase extends Component {
     const AccountCreatedAt = this.props.firebase.createdAt();
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
-      .then(authUser => {
+      .then((authUser) => {
         // Create a user in your Real Time Database
-        this.props.firebase
-          .userRealDb(authUser.user.uid)
-          .set({
-            username,
-            challengesCompleted,
-            lastCorrectAnswerAtRealDb,
-            timeStampAndChallengeCompleted
-          })
-          // Create a user in your Firestore
-        return this.props.firebase
-          .user(authUser.user.uid)
-          .set({
+        this.props.firebase.userRealDb(authUser.user.uid).set({
+          username,
+          challengesCompleted,
+          lastCorrectAnswerAtRealDb,
+          timeStampAndChallengeCompleted,
+        });
+        // Create a user in your Firestore
+        return this.props.firebase.user(authUser.user.uid).set(
+          {
             username,
             email,
             AccountCreatedAt,
@@ -67,23 +64,22 @@ class SignUpFormBase extends Component {
             challengesCompleted,
             lastCorrectAnswerAt,
           },
-          { merge: true },
-          )
-        
+          { merge: true }
+        );
       })
-      
+
       .then(() => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({ error });
         this.setState({ loading: false });
       });
     event.preventDefault();
   };
 
-  onChange = event => {
+  onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
@@ -94,77 +90,97 @@ class SignUpFormBase extends Component {
       passwordOne,
       passwordTwo,
       error,
-      loading
+      loading,
     } = this.state;
 
     const isInvalid =
       passwordOne !== passwordTwo ||
-      passwordOne === '' ||
-      email === '' ||
-      username === '';
+      passwordOne === "" ||
+      email === "" ||
+      username === "";
 
     return (
       <form onSubmit={this.onSubmit} className="formcontainer">
-
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-          required />
-          <label for="name" className="form__label">Username</label>
+          <input
+            type="input"
+            className="form__field"
+            name="username"
+            value={username}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Full Name"
+            required
+          />
+          <label for="name" className="form__label">
+            Username
+          </label>
         </div>
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-          required />
-          <label for="name" className="form__label">Email Address</label>
+          <input
+            type="input"
+            className="form__field"
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+            required
+          />
+          <label for="name" className="form__label">
+            Email Address
+          </label>
         </div>
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-          required />
-          <label for="name" className="form__label">Password</label>
+          <input
+            type="input"
+            className="form__field"
+            name="passwordOne"
+            value={passwordOne}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <label for="name" className="form__label">
+            Password
+          </label>
         </div>
         <div className="form__group field">
-          <input type="input"
-          className="form__field"
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-          required />
-          <label for="name" className="form__label">Confirm Password</label>
+          <input
+            type="input"
+            className="form__field"
+            name="passwordTwo"
+            value={passwordTwo}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Confirm Password"
+            required
+          />
+          <label for="name" className="form__label">
+            Confirm Password
+          </label>
         </div>
-        <button disabled={isInvalid} className="button__form__submit" type="submit">Sign Up <ClipLoader size={8} color={'#4CB8A4'} loading={loading}/></button>
         {error && <p className="error__text caution__text">{error.message}</p>}
+        <button
+          disabled={isInvalid}
+          className="button__form__submit"
+          type="submit"
+        >
+          Sign Up <ClipLoader size={8} color={"#4CB8A4"} loading={loading} />
+        </button>
       </form>
     );
   }
 }
 const SignInLink = () => (
   <p className="form__bottom__links">
-    Already have an account?  <Link to={ROUTES.SIGN_IN} className="link__decoration">
-    <span className="highlighted__text">SIGN IN</span></Link>
+    Already have an account?{" "}
+    <Link to={ROUTES.SIGN_IN} className="link__decoration">
+      <span className="highlighted__text">Login</span>
+    </Link>
   </p>
 );
-const SignUpForm = compose(
-  withRouter,
-  withFirebase,
-)(SignUpFormBase);
+const SignUpForm = compose(withRouter, withFirebase)(SignUpFormBase);
 export default SignUpPage;
 export { SignUpForm, SignInLink };
